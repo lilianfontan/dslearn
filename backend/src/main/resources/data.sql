2b69dbd0e52152afd52ade8f0bc72434d7129b96
lilianfontan <lilian.fontan@gmail.com>
2023-02-28 19:38:47 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,3 +13,4 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIeiEwQE6JNskRRgaymVKKAbK33nKb49JX7w&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThz2V92NxLwz-yepQsgWTBSPsTFMTAOSaKvA&usqp=CAU');
